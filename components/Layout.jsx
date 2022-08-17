26bd9d63414cbe2d2c88285fcd544d4d4f995ad9
Tigo S Yoga <tigo.yoga@gmail.com>
2022-08-17 19:42:28 +0700

    fix: disable scroll when menu active

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, showNav, setShowNav }) => {
   console.log(children);
   return (
     <div>
-      <Navbar />
+      <Navbar showNav={showNav} setShowNav={setShowNav} />
       {children}
       <Footer />
     </div>
