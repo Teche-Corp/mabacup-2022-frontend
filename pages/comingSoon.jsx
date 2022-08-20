8906a43a1c7505a3fc8ed4904fe3fb6837c90314
Thariq Agfi Hermawan <thoriq.aghfi60@gmail.com>
2022-08-20 18:48:52 +0700

    fix: comingSoon page

--- pages/comingSoon.jsx
+++ pages/comingSoon.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
-
+import { FaInstagram, FaTiktok, FaYoutube} from "react-icons/fa";
+import {BsLine} from "react-icons/bs";
 export default function comingSoon() {
   return (
     <>
@@ -9,7 +10,25 @@ export default function comingSoon() {
           <Link href='/'>
             <button className="dive-button rounded-full px-6 py-3 hover:scale-105 text-white font-secondary font-medium mt-10">Return to Home</button>
           </Link>
-          <p className="text-white font-secondary pt-40">Stay tune on :</p>
+          <p className="text-white font-secondary pt-40 relative">Stay tune on :</p>
+          <div className="flex mt-10 w-2/5 justify-between">
+            <div className="flex gap-2 items-center">
+              <FaInstagram className="text-white" size={28}/>
+              <p className="text-white font-secondary">@mabacupits</p>
+            </div>
+            <div className="flex gap-2 items-center">
+              <FaTiktok className="text-white" size={28}/>
+              <p className="text-white font-secondary">mabacupits</p>
+            </div>
+            <div className="flex gap-2 items-center">
+              <FaYoutube className="text-white" size={28}/>
+              <p className="text-white font-secondary">MABACUP ITS</p>
+            </div>
+            <div className="flex gap-2 items-center">
+              <BsLine className="text-white" size={28}/>
+              <p className="text-white font-secondary">@899jpfck</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
