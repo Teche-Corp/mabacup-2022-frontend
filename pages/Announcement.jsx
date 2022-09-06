72c7948616fca2ba5b09b572ce091b823bb103f7
Thariq Agfi Hermawan <thoriq.aghfi60@gmail.com>
2022-09-07 05:26:07 +0700

    fix: Ditolak implement

--- pages/Announcement.jsx
+++ pages/Announcement.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Announcement() {
   const [value, setValue] = useState("");
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(3);
   const [data, setData] = useState({});
   const handleClick = () => {
     toast.promise(
@@ -15,10 +15,10 @@ export default function Announcement() {
         .then((res) => {
           if (res.status === 200) {
             setPage(page + 1);
-
             setData(res?.data.data);
-          } else if (res.status === 200) {
+          } else {
             setPage(page + 2);
+            setData(res?.data.data);
           }
         }),
       {
@@ -169,18 +169,21 @@ export default function Announcement() {
             </p>
             <button
               onClick={handleKembali}
-              className='px-4 py-3 rounded-lg mt-8 bg-[#5189C4] text-white'
+              className='px-3 py-2 rounded-lg mt-8 bg-[#5189C4] text-white border-2 border-[#5189C4] hover:text-[#5189C4] hover:bg-white'
             >
               Kembali ke laman pencarian
             </button>
           </div>
         </div>
 
+
+
+
         {/* Ditolak */}
         <div
           className={`${
             page === 3 ? "block" : "hidden"
-          } w-3/4 h-3/5 mx-auto my-auto bg-white rounded-xl overflow-hidden`}
+          } w-4/5 h-[55%] mx-auto my-auto bg-white rounded-xl overflow-hidden`}
         >
           <div className='bg-[#FFCECB] h-20 px-6 py-6'>
             <h1 className='text-[#CB3A31] font-secondary text-2xl font-semibold '>
@@ -194,36 +197,37 @@ export default function Announcement() {
             <div>
               <table className='table-auto border-spacing-4 p-4 mt-4'>
                 <tr>
-                  <td className='text-neutral-400'>NRP</td>
-                  <td className='text-neutral-400'>
+                  <td className='text-neutral-500'>NRP</td>
+                  <td className='text-neutral-500'>
                     :{" "}
                     <span className='text-black font-semibold'>{data.nrp}</span>
                   </td>
                 </tr>
                 <tr>
-                  <td className='text-neutral-400'>
+                  <td className='text-neutral-500'>
                     Asal Departemen &nbsp; &nbsp;
                   </td>
-                  <td className='text-neutral-400'>
+                  <td className='text-neutral-500'>
                     :{" "}
                     <span className='text-black font-semibold'>
                       {data.departemen}
                     </span>
                   </td>
                 </tr>
-                <tr>
-                  <td className='text-neutral-400'>Divisi / Subdivisi</td>
-                  <td className='capitalize text-neutral-400'>
-                    :{" "}
-                    <span className='text-black font-semibold'>
-                      {data.divisi_diterima}
-                    </span>
-                  </td>
-                </tr>
               </table>
             </div>
+            <p className='mt-4 text-base text-[#404040]'>
+            Kamu hebat karena mau mencoba! Terimakasih telah tertarik dan menunjukkan performa terbaik di Maba Cup 2022. Kami tunggu kontribusi kamu di event Maba Cup selanjutnya..
+              <br />
+            </p>
+            <button
+              onClick={handleKembali}
+              className='px-4 py-3 rounded-lg mt-28 md:mt-32 lg:mt-40 bg-[#5189C4] text-white border-2 border-[#5189C4] hover:text-[#5189C4] hover:bg-white'
+            >
+              Kembali ke laman pencarian
+            </button>
           </div>
-        </div>
+        </div>      
       </div>
     </>
   );
