0b2331945eebe830f14fa7ba54ddbb7273ccec6d
Robby Pambudi <5025211042@mhs.its.ac.id>
2022-10-15 16:09:21 +0700

    fix: fixing mising link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = ({ showNav, setShowNav }) => {
   return (
     <>
       <ToastContainer
-        position='top-center'
+        position="top-center"
         autoClose={1000}
         hideProgressBar={false}
         newestOnTop={false}
@@ -57,7 +57,7 @@ const Navbar = ({ showNav, setShowNav }) => {
         draggable
         pauseOnHover
       />
-      <div className='fixed top-0 z-50'>
+      <div className="fixed top-0 z-50">
         <nav
           className={`${
             colorChange
@@ -66,50 +66,50 @@ const Navbar = ({ showNav, setShowNav }) => {
           } h-20 relative z-50 select-none w-screen font-secondary text-white  flex justify-between items-center px-5 md:px-16 lg:px-24`}
         >
           {/* header logo */}
-          <Link href='/'>
-            <a className='md:hidden opacity-0 relative z-50 flex gap-1'>
-              <div className='flex flex-col justify-center'>
-                <img src='/mabacup.svg' alt='' />
+          <Link href="/">
+            <a className="md:hidden opacity-0 relative z-50 flex gap-1">
+              <div className="flex flex-col justify-center">
+                <img src="/mabacup.svg" alt="" />
               </div>
             </a>
           </Link>
-          <Link href='/'>
-            <a className='relative z-50 flex gap-1'>
-              <div className='flex flex-col justify-center'>
-                <img src='/mabacup.svg' alt='' />
+          <Link href="/">
+            <a className="relative z-50 flex gap-1">
+              <div className="flex flex-col justify-center">
+                <img src="/mabacup.svg" alt="" />
               </div>
             </a>
           </Link>
-          <div className='relative z-50 flex mr-2 md:hidden'>
+          <div className="relative z-50 flex mr-2 md:hidden">
             {!showNav && (
               <GiHamburgerMenu
-                className='text-4xl text-white'
+                className="text-4xl text-white"
                 onClick={toggleShowNav}
               />
             )}
             {showNav && (
               <FaTimes
-                className='text-4xl text-red-600'
+                className="text-4xl text-red-600"
                 onClick={toggleShowNav}
               />
             )}
           </div>
           <ul className={`md:flex hidden  md:gap-5 lg:gap-10 xl:gap-[67px] `}>
             <li>
-              <Link href='/'>
+              <Link href="/">
                 <a>Home</a>
               </Link>
             </li>
 
             <li>
-              <Link href='/AboutUs'>
+              <Link href="/AboutUs">
                 <a>About</a>
               </Link>
             </li>
-            <li className='relative'>
+            <li className="relative">
               <button
                 onClick={toggleShowEvents}
-                className='flex items-center gap-[6px]'
+                className="flex items-center gap-[6px]"
               >
                 Events{" "}
                 <FaAngleDown
@@ -125,11 +125,11 @@ const Navbar = ({ showNav, setShowNav }) => {
                     showEvents ? "opacity-1" : "opacity-0 top-0"
                   } right-0 w-32 flex flex-col p-1 py-2 text-secondary transition duration-300 bg-white text-slate-900 top-8`}
                 >
-                  <div className='flex flex-col w-full gap-2'>
-                    <div onClick={(toggleShowEvents, notify)}>
+                  <div className="flex flex-col w-full gap-2">
+                    <div>
                       <Link href={"/preEvent"}>
-                        <a className='text-slate-900'>
-                          <div className='flex items-start justify-center gap-4 rounded-sm hover:bg-slate-100 '>
+                        <a className="text-slate-900">
+                          <div className="flex items-start justify-center gap-4 rounded-sm hover:bg-slate-100 ">
                             <h2>Pre Event</h2>
                           </div>
                         </a>
@@ -137,18 +137,18 @@ const Navbar = ({ showNav, setShowNav }) => {
                     </div>
 
                     <div>
-                      <Link href='/MainEvent'>
-                        <a className='text-slate-900'>
-                          <div className='flex items-start justify-center gap-4 rounded-sm hover:bg-slate-100 '>
+                      <Link href="/MainEvent">
+                        <a className="text-slate-900">
+                          <div className="flex items-start justify-center gap-4 rounded-sm hover:bg-slate-100 ">
                             <h2>Main Event</h2>
                           </div>
                         </a>
                       </Link>
                     </div>
                     <div onClick={(toggleShowEvents, notify)}>
-                      <Link href='/'>
-                        <a className='text-slate-900'>
-                          <div className='flex items-start justify-center gap-4 rounded-sm hover:bg-slate-100 '>
+                      <Link href="/">
+                        <a className="text-slate-900">
+                          <div className="flex items-start justify-center gap-4 rounded-sm hover:bg-slate-100 ">
                             <h2>Closing</h2>
                           </div>
                         </a>
@@ -159,41 +159,41 @@ const Navbar = ({ showNav, setShowNav }) => {
               )}
             </li>
             <li>
-              <Link href='/comingSoon'>
+              <Link href="/comingSoon">
                 <a>Merchandise</a>
               </Link>
             </li>
           </ul>
 
-          <Link href={"https://mabacup-its.com/oprec-ambassador"}>
+          {/* <Link href={"https://mabacup-its.com/oprec-ambassador"}>
             <button className='invisible oprec-button hidden md:block w-48 lg:w-48 h-[2.5rem] font-medium rounded-lg text-white text-sm '>
               Open Recruitment
             </button>
-          </Link>
+          </Link> */}
 
           <ul
             className={`flex md:hidden translate-y-full ${
               showNav ? "translate-x-0 opacity-1" : "translate-x-full opacity-0"
             } text-white z-20 transition duration-300 flex-col h-screen bg-[#27378E] absolute bottom-0 left-0  w-full`}
           >
-            <li onClick={toggleShowNav} className='py-4 px-7'>
-              <Link href='/'>
-                <a className='flex items-center justify-center w-full text-2xl font-secondary text-border-thin '>
+            <li onClick={toggleShowNav} className="py-4 px-7">
+              <Link href="/">
+                <a className="flex items-center justify-center w-full text-2xl font-secondary text-border-thin ">
                   Home
                 </a>
               </Link>
             </li>
-            <li onClick={toggleShowNav} className='py-4  px-7'>
-              <Link href='/AboutUs'>
-                <a className='flex items-center justify-center w-full text-2xl font-secondary text-border-thin '>
+            <li onClick={toggleShowNav} className="py-4  px-7">
+              <Link href="/AboutUs">
+                <a className="flex items-center justify-center w-full text-2xl font-secondary text-border-thin ">
                   About
                 </a>
               </Link>
             </li>
-            <li className='relative py-4 px-7'>
+            <li className="relative py-4 px-7">
               <button
                 onClick={toggleShowEvents}
-                className='flex gap-2 items-center justify-center w-full text-2xl font-secondary text-border-thin  '
+                className="flex gap-2 items-center justify-center w-full text-2xl font-secondary text-border-thin  "
               >
                 Events{" "}
                 <FaAngleDown
@@ -206,28 +206,16 @@ const Navbar = ({ showNav, setShowNav }) => {
             <div
               className={`${showEvents ? "flex" : "hidden"} flex-col w-full`}
             >
-              <div
-                onClick={() => {
-                  toggleShowEvents();
-                  toggleShowNav();
-                }}
-                className='flex h-10 gap-5  px-7'
-              >
-                <Link href='/comingSoon'>
-                  <a className='inline-block flex items-center justify-center w-full text-xl  font-secondary'>
+              <div className="flex h-10 gap-5  px-7">
+                <Link href="/preEvent">
+                  <a className="inline-block flex items-center justify-center w-full text-xl  font-secondary">
                     Pre Event
                   </a>
                 </Link>
               </div>
-              <div
-                onClick={() => {
-                  toggleShowEvents();
-                  toggleShowNav();
-                }}
-                className='flex h-10 gap-5 px-7'
-              >
-                <Link href='/'>
-                  <a className='inline-block flex items-center justify-center w-full text-xl  font-secondary'>
+              <div className="flex h-10 gap-5 px-7">
+                <Link href="/MainEvent">
+                  <a className="inline-block flex items-center justify-center w-full text-xl  font-secondary">
                     Main Event
                   </a>
                 </Link>
@@ -237,23 +225,23 @@ const Navbar = ({ showNav, setShowNav }) => {
                   toggleShowEvents();
                   toggleShowNav();
                 }}
-                className='flex h-10 gap-5 px-7'
+                className="flex h-10 gap-5 px-7"
               >
-                <Link href='/'>
-                  <a className='inline-block flex items-center  justify-center w-full text-xl  font-secondary'>
+                <Link href="/">
+                  <a className="flex items-center  justify-center w-full text-xl  font-secondary">
                     Closing
                   </a>
                 </Link>
               </div>
             </div>
             <li onClick={toggleShowNav} className={`py-4 px-7`}>
-              <Link href='/'>
-                <a className='flex items-center justify-center w-full  text-2xl font-secondary text-border-thin  md:font-secondary '>
+              <Link href="/">
+                <a className="flex items-center justify-center w-full  text-2xl font-secondary text-border-thin  md:font-secondary ">
                   Merchandise
                 </a>
               </Link>
             </li>
-            <li
+            {/* <li
               onClick={toggleShowNav}
               className='absolute bottom-52 translate-x-1/2 right-1/2'
             >
@@ -262,30 +250,30 @@ const Navbar = ({ showNav, setShowNav }) => {
                   Open Recruitment
                 </button>
               </Link>
-            </li>
+            </li> */}
             <li
               onClick={toggleShowNav}
-              className='absolute bottom-32 right-1/2 translate-x-1/2 mt-4 flex gap-8 justify-center items-center'
+              className="absolute bottom-32 right-1/2 translate-x-1/2 mt-4 flex gap-8 justify-center items-center"
             >
               <Link href={"#"}>
                 <a>
-                  <img src='/instagram.svg' alt='' />
+                  <img src="/instagram.svg" alt="" />
                 </a>
               </Link>
               <Link href={"#"}>
                 <a>
-                  <img src='/youtube.svg' alt='' />
+                  <img src="/youtube.svg" alt="" />
                 </a>
               </Link>
 
               <Link href={"#"}>
                 <a>
-                  <img src='/tiktok.svg' alt='' />
+                  <img src="/tiktok.svg" alt="" />
                 </a>
               </Link>
               <Link href={"#"}>
                 <a>
-                  <img src='/line.svg' alt='' />
+                  <img src="/line.svg" alt="" />
                 </a>
               </Link>
             </li>
